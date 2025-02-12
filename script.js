1173e35a7f882e4af9e7f45a90f2c5a843017a17
Broxxiie <broquezachristian@gmail.com>
2025-02-12 22:12:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cursor = document.querySelector(".cursor");
 
 window.addEventListener("mousemove", function (e) {
-  cursor.style.left = e.clientX - "px" + "px";
-  cursor.style.top = e.clientY - "px" + "px";
+  cursor.style.left = e.clientX - 100 + "px";
+  cursor.style.top = e.clientY - 100 + "px";
 });
